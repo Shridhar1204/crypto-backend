53baff6ecac06690fe8a4caf1602669b0dc14eb1
Shridhar1204 <sharmashridhar1204@gmail.com>
2025-05-06 00:56:03 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+require('./Models/db');
+const bodyParser = require("body-parser")
+const AuthRouter = require('./Routes/AuthRouter');
+const HoldingRouter = require('./Routes/HoldingRouter');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use('/auth', AuthRouter);
+app.use('/holdings', HoldingRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
